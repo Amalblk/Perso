eb38c319cc437524a5405003f459a2db5ead6403
Amal TAIROU <amaltairou29@gmail.com>
2025-08-29 22:23:13 -0400

    Update submit-form.js

--- api/submit-form.js
+++ api/submit-form.js
@@ -3,9 +3,10 @@ import { google } from "googleapis";
 import nodemailer from "nodemailer";
 
 function setCORS(res) {
-  res.setHeader("Access-Control-Allow-Origin", "https://amaltairou.framer.website"); // 👈 safer than *
-  res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+res.setHeader("Access-Control-Allow-Origin", "https://amaltairou.framer.website");
+res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
+res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+if (req.method === "OPTIONS") return res.status(200).end();
 }
 
 export default async function handler(req, res) {
